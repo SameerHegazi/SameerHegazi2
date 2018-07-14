6272048cafc4b81a4f76cf9c95b2195c3ae9ee53
SameerHegazi <40927890+SameerHegazi@users.noreply.github.com>
2018-07-14 04:45:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -287,18 +287,18 @@ ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
  client.on('message', message => {
               if(!message.channel.guild) return;
     var prefix = "$";
-    if(message.content.startsWith('$dont')) {
+    if(message.content.startsWith('$bc')) {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
     let copy = "ARK";
     let request = `Requested By ${message.author.username}`;
     if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
-    msg.react('✅')
+    msg.react('❌')
     .then(() => msg.react('❌'))
-    .then(() =>msg.react('✅'))
+    .then(() =>msg.react('❌'))
 
-    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
     let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
        let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
     let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
