c9d79ca420a711c2a82701630e864671573946a5
SameerHegazi <40927890+SameerHegazi@users.noreply.github.com>
2018-07-23 22:43:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2330,7 +2330,7 @@ client.on('message', message => {
      let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setColor("#9B59B6")
-  .addField(" ** :gear: Server Support :gear: **" , "  **https://discord.gg/zTZtmXC**")
+  .addField(" ** :gear: Server Support :gear: **" , "  **https://discord.gg/GrHcjDF**")
      
      
   message.channel.sendEmbed(embed);
