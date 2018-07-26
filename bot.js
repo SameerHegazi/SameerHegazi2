262a5b4dddf2ca22f3c883109eb885d9c8807e3e
SameerHegazi <40927890+SameerHegazi@users.noreply.github.com>
2018-07-26 16:24:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '!.'
+const prefix = '-_'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -230,15 +230,15 @@ if (message.content.startsWith(prefix + 'help')) {
 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 ╔[❖══════════════════════❖]╗
 | لمسح عدد معين من الـرسائل |
-| ---    !.clear    ---- |
+| ---    -_حذف    ---- |
 ╚[❖══════════════════════❖]╝
 ╔[❖════════════════════════❖]╗
 | لإغلاق الرومات وجعلها للمشرفين |
 | ---    !.closeroms   ---- |
 ╚[❖══════════════════════❖]╝
 ╔[❖══════════════════════❖]╗
-| إغلاق الرومات وجعلها للمشرفين |
-| ---    !.openroms   ---- |
+| لعمل برودكاست |
+| ---    -_bc   ---- |
 ╚[❖══════════════════════❖]╝
 **
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -296,6 +296,28 @@ msg.delete();
 })
 })
 }
+});
+client.on("message", message => {
+    var prefix = "-_"; // غير هنا حط البرفكس
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "حذف")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **ليس لديك صلاحيات**');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم",
+        color: 0x06DF00,
+        description: "تم مسح الرسائل بنجاح",
+        footer: {
+          text: "Alloo" // غير هنا حط اسم البوت
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+
+     
 });
 
 client.login(process.env.BOT_TOKEN);
